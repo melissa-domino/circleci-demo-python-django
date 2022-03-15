9669cf80d201164ee629b710e2e944a08b563c6f
Melissa Xu <melissa.xu@dominodatalab.com>
2022-03-14 21:09:06 -0700

    Pushed from Domino: /u/cheng.liu/DemoProjectBankMarketing/runs/6230076475673628843c5bdb

--- manage.py
+++ manage.py
@@ -5,8 +5,7 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "locallibrary.settings")
     try:
-        from django.core.management import execute
-        #test for fail execute_from_command_line
+        from django.core.management import execute_from_command_line
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
